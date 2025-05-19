a936dfef41d5b02f74435a012eb13208065fa798
Dim <dimcegnlm@gmail.com>
2025-05-19 15:24:25 +0300

    refactor: add group in game loop

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ from constants import *
 from player import Player
 
 
-
 def main():
     pygame.init
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+    # create 2 groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # make the 2 group a playerclass
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        # player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+        # player.draw(screen)
         pygame.display.flip()
 
         #limit frames to 60
