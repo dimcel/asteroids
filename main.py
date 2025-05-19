c080f3e5f265fbc317e6beb0e278034955e96614
Dim <dimcegnlm@gmail.com>
2025-05-19 14:36:39 +0300

    add movement and triange

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
-pygame.init
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# while True:
-#     screen.fill("black")
-#     pygame.display.flip()
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             return
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        #limit frames to 60
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
